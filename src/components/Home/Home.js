9c33954559a0532337a248445113ecf396c9c78c
monirulislam9549 <monirulislam9549@gmail.com>
2022-04-05 12:51:41 +0600

    button added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import image1 from './keyboard1.jpg';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-            <h1>This is home</h1>
+        <div className='keyboard-text'>
+            <div className='text'>
+                <h1>
+                    <span style={{ color: 'blue' }} >Keychorn Keyboard is a Beast</span>
+                    <br />
+                    <span style={{ color: 'green' }}>It has Ultra Fast Technology </span>
+                </h1>
+                <br />
+                <p>
+                    Keychron K2 is a 75% layout (84-keys) white backlit compact Bluetooth mechanical keyboard. The ultimate keyboard that retains shortcut and arrow keys.
+                </p>
+                <br />
+                <button className='detail-btn'>See Details</button>
+            </div>
+            <div>
+                <img src={image1} alt="" height={'600px'} />
+            </div>
         </div>
+
     );
 };
 
